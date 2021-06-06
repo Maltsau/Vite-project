ba7e15cf81154b0499ded7e946ece3cd48c01fd1
hobotok85 <s_a_m_85@tut.by>
2021-06-06 20:06:05 +0300

    Как исправить генератор?

--- src/services/generator.ts
+++ src/services/generator.ts
@@ -1,3 +1,7 @@
 export default function randomize(numberOfDigits: string) {
-  return String(Math.trunc(Math.random() * 10 ** Number(numberOfDigits)));
-}
+  let stringLength = String(Math.trunc(Math.random() * 10 ** Number(numberOfDigits)));
+  if (stringLength.length === Number(numberOfDigits)){
+  console.log (stringLength)  
+  return stringLength;}
+  else (console.log ("Ошибка"+ stringLength))
+  ;}
