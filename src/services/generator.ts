d2bb2f15da4e1873ca6f518a0e0fd3fd1cd03541
hobotok85 <s_a_m_85@tut.by>
2021-06-06 21:25:36 +0300

    И так не прокатывает

--- src/services/generator.ts
+++ src/services/generator.ts
@@ -1,7 +1,6 @@
 export default function randomize(numberOfDigits: string) {
   let stringLength = String(Math.trunc(Math.random() * 10 ** Number(numberOfDigits)));
   if (stringLength.length === Number(numberOfDigits)){
-  console.log (stringLength)  
-  return stringLength;}
-  else (console.log ("Ошибка"+ stringLength))
+    return stringLength}
+  else {randomize (numberOfDigits);}
   ;}
