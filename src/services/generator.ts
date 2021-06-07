2f9eb9911eca823ff870f2ea5825c2dafb5cab6f
Maltsau <Maltsau@users.noreply.github.com>
2021-06-07 19:37:21 +0300

    newGenerator

--- src/services/generator.ts
+++ src/services/generator.ts
@@ -1,15 +1,3 @@
-// export default function randomize(numberOfDigits: string) {
-//   return String(Math.trunc(Math.random() * 10 ** Number(numberOfDigits)));
-// }
-
-// export default function randomize(numberOfDigits: string) {
-//   let stringLength = String(Math.trunc(Math.random() * 10 ** Number(numberOfDigits)));
-//   if (stringLength.length === Number(numberOfDigits)){
-//     console.log (stringLength)
-//     return stringLength;
-//   }
-//   else (console.log ("Ошибка"+ stringLength))
-// }
 
 function randomNumberBetween(min: number, max: number) {
   let randomNum = Math.random() * (max - min) + min;
@@ -17,13 +5,6 @@ function randomNumberBetween(min: number, max: number) {
 }
 
 export default function (numberOfDigits: string) {
-  // calculate min and max
-  const random = randomNumberBetween(1_000_000, 9_999_999);
+  const random = randomNumberBetween(10**(Number(numberOfDigits)-1), 10**Number(numberOfDigits)-1); 
   return String(random);
-  // while (true) {
-  //   let num = String(Math.trunc(Math.random() * 10 ** Number(numberOfDigits)));
-  //   if (num.length === Number(numberOfDigits)) {
-  //     return num;
-  //   } else console.log("Eror ", num);
-  // }
 }
